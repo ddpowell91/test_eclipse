50e1bbd53fb0cc270ec007cf2f580a8f5720e0fa
David Powell <ddpowell91@gmail.com>
2018-08-22 22:00:00 -0400

    FIXED

--- Project1/src/dev1/HelloWorld.java
+++ Project1/src/dev1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello Everyone");
+			System.out.println("Hello Daves of the World");
 	}
 
 }
